812834d02a3858e5b53befcc2f71cc15eb0f5e42
SlimSpeedster <yashdgandhi@outlook.com>
2021-04-28 17:06:17 -0400

    help

--- sketch.js
+++ sketch.js
@@ -1,63 +1,30 @@
-var starImg,bgImg;
-var star, starBody;
-//create variable for fairy sprite and fairyImg
+var canvas, backgroundImage;
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
 
-function preload()
-{
-	starImg = loadImage("images/star.png");
-	bgImg = loadImage("images/starNight.png");
-	//load animation for fairy here
-}
-
-function setup() {
-	createCanvas(800, 750);
-
-	//write code to play fairyVoice sound
-
-	//create fairy sprite and add animation for fairy
+var question, contestant, quiz;
 
 
-	star = createSprite(650,30);
-	star.addImage(starImg);
-	star.scale = 0.2;
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
-	World.add(world, starBody);
-	
-	Engine.run(engine);
-
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
 }
 
 
-function draw() {
-  background(bgImg);
-
-  star.x= starBody.position.x 
-  star.y= starBody.position.y 
-
-  console.log(star.y);
-
-  //write code to stop star in the hand of fairy
-
-  drawSprites();
-
-}
-
-function keyPressed() {
-
-	if (keyCode === DOWN_ARROW) {
-		Matter.Body.setStatic(starBody,false); 
-	}
-
-	//writw code to move fairy left and right
-	
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
 }
